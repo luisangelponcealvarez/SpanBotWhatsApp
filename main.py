ca74c9302d63e5e7484159c6945ffab9d393d076
Luis Angel Ponce Alvarez <poncealvarezluisangel4@gmail.com>
2023-08-05 19:41:11 -0600

    añado archivos del script

---
+++ main.py
@@ -0,0 +1,9 @@
+import pyautogui, webbrowser
+from time import sleep
+
+webbrowser.open("https://web.whatsapp.com/send?phone=+xxxxxxxxxx)
+sleep(30)
+with open("spam.txt", "r") as file:
+    for line in file:
+        pyautogui.typewrite(line)
+        pyautogui.press("enter")
